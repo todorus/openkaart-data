823a391fc3bc7a883413fb3a63e051c7419eeb46
Theo van den Bogaart <todorus@todorus.com>
2016-11-05 18:28:08 +0100

    moving scraper stuff in seperate dir

--- scrapers/run_scrapers.py
+++ scrapers/run_scrapers.py
@@ -10,12 +10,12 @@ ch.setFormatter(logging.Formatter(fmt='%(asctime)s %(message)s'))
 logging.getLogger().addHandler(ch)
 
 cbsRegionsWFS = 'https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/ows'
-# WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_provincie_2016_gegeneraliseerd', '../data/provinces.geo.json', None).start()
-MunicipalityWFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', '/Volumes/openkaart_data/municipalities.geo.json', None).start()
+WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_provincie_2016_gegeneraliseerd', '/Volumes/openkaart_data/provinces.geo.json', None).start()
+WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', '/Volumes/openkaart_data/municipalities.geo.json', None).start()
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_wijk_2016_gegeneraliseerd', '../data/district.geo.json', None).start()
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_buurt_2016_gegeneraliseerd', '../data/neighborhoods.geo.json', None).start()
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_ggdregio_2016_gegeneraliseerd', '../data/ggd.geo.json', None).start()
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_jeugdzorgregio_2016_gegeneraliseerd', '../data/youthcare.geo.json', None).start()
 
-bagAddressWFS = 'https://geodata.nationaalgeoregister.nl/inspireadressen/wfs'
-AddressWFSScraper(bagAddressWFS, 'inspireadressen:inspireadressen', '/Volumes/openkaart_data/adressen.geo.json', None).start()
+# bagAddressWFS = 'https://geodata.nationaalgeoregister.nl/inspireadressen/wfs'
+# AddressWFSScraper(bagAddressWFS, 'inspireadressen:inspireadressen', '/Volumes/openkaart_data/adressen.geo.json', None).start()
